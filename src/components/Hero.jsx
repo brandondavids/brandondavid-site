6d6a71f9ddec06324d3dd28e17ffa7d9dc9e11be
brandondavids <bdsinambela@gmail.com>
2025-11-23 09:03:57 +0700

    more fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,12 +33,12 @@ export default function Hero() {
                 sm:max-w-[70%]
 
                 md:absolute md:top-[4vh]
-                md:right-20
-                md:max-w-[540px]
+                md:right-15
+                md:max-w-[860px]
                 md:text-right
             ">
                 <p className="
-                    font-poppins font-light
+                    font-poppins font-light tracking-tighter
                     text-[clamp(1.2rem,2.2vw,3.4rem)]
                 ">
                     I like figuring out how things work. Whether it's gaming, code, tools, or some random idea that shows up at 2 a.m. If something doesn't make sense, I'll probably poke at it until it does.
@@ -48,11 +48,11 @@ export default function Hero() {
             <div className="
                 w-full flex justify-center pb-[env(safe-area-inset-bottom)]
                 md:absolute md:bottom-[-4vh] 
-                md:left-[-2vw] md:justify-start
+                md:left-[-1px] md:justify-start
             ">
                 <h1 className="
                     font-serifbrand font-bold leading-none tracking-tighter
-                    text-[clamp(4rem,18vw,32rem)]
+                    text-[clamp(8.5rem,22vw,45rem)]
                 ">davids</h1>
             </div>
         </section>
