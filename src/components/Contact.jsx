59005f06e6ebdc7e17d26de9e9ad6d56d886ef1a
brandondavids <bdsinambela@gmail.com>
2025-11-26 11:20:00 +0700

    revert

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,54 +1,54 @@
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-// export default function Contact() {
-//     const [scrollY, setScrollY] = useState(0);
+export default function Contact() {
+    const [scrollY, setScrollY] = useState(0);
     
-//         useEffect(() => {
-//             const handleScroll = () => {
-//               setScrollY(window.scrollY);  
-//             };
+        useEffect(() => {
+            const handleScroll = () => {
+              setScrollY(window.scrollY);  
+            };
     
-//             window.addEventListener("scroll", handleScroll);
-//             return () => window.removeEventListener("scroll", handleScroll);
-//         })
+            window.addEventListener("scroll", handleScroll);
+            return () => window.removeEventListener("scroll", handleScroll);
+        })
     
-//     const offset = Math.max(0, 300 - scrollY); 
-//     const opacity = Math.min(scrollY / 200, 1);
+    const offset = Math.max(0, 300 - scrollY); 
+    const opacity = Math.min(scrollY / 200, 1);
 
-//     return (
-//         <section className="min-h-dvh text-black relative -mt-[250px] flex flex-col items-center justify-center"
-//             style={{
-//                 transform: `translateY(${offset}px)`,
-//                 opacity,
-//                 transition: "transform 0.1s linear, opacity 0.1s linear",
-//             }}>
+    return (
+        <section className="min-h-dvh text-black relative -mt-[250px] flex flex-col items-center justify-center"
+            style={{
+                transform: `translateY(${offset}px)`,
+                opacity,
+                transition: "transform 0.1s linear, opacity 0.1s linear",
+            }}>
 
-//             <div className="text-center">
-//                 <h2 className="font-poppins text-[clamp(1.2rem,2.4vw,3rem)] tracking-tighter leading-tight">
-//                     Brandon David
-//                 </h2>
+            <div className="text-center">
+                <h2 className="font-poppins text-[clamp(1.2rem,2.4vw,3rem)] tracking-tighter leading-tight">
+                    Brandon David
+                </h2>
 
-//                 <p className="font-poppins text-[clamp(1.2rem,2.4vw,3rem)] tracking-tighter leading-tight">
-//                     brdndvid@gmail.com
-//                 </p>
+                <p className="font-poppins text-[clamp(1.2rem,2.4vw,3rem)] tracking-tighter leading-tight">
+                    brdndvid@gmail.com
+                </p>
 
-//                 <div className="pt-6 space-y-2">
-//                     <a
-//                         href="https://github.com/brandondavids"
-//                         className="block text-[clamp(1.2rem,2.4vw,3rem)] underline underline-offset-4 decoration-[2px] font-poppins tracking-tighter leading-tight"
-//                         target="_blank"
-//                     >
-//                         Github
-//                     </a>
-//                     <a
-//                         href="https://instagram.com/ddviidd"
-//                         className="block text-[clamp(1.2rem,2.4vw,3rem)] underline underline-offset-4 decoration-[2px] font-poppins tracking-tighter leading-tight"
-//                         target="_blank"
-//                     >
-//                         Instagram
-//                     </a>
-//                 </div>
-//             </div>
-//         </section>
-//     )
-// }
+                <div className="pt-6 space-y-2">
+                    <a
+                        href="https://github.com/brandondavids"
+                        className="block text-[clamp(1.2rem,2.4vw,3rem)] underline underline-offset-4 decoration-[2px] font-poppins tracking-tighter leading-tight"
+                        target="_blank"
+                    >
+                        Github
+                    </a>
+                    <a
+                        href="https://instagram.com/ddviidd"
+                        className="block text-[clamp(1.2rem,2.4vw,3rem)] underline underline-offset-4 decoration-[2px] font-poppins tracking-tighter leading-tight"
+                        target="_blank"
+                    >
+                        Instagram
+                    </a>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,46 +15,46 @@ export default function Hero() {
     const blur = Math.min(scrollY / 40, 12);
     const fade = Math.max(1 - scrollY / 500, 0);
 
-//    return (
-        // <section className="
-        //         min-h-dvh bg-[#eeeee0] px-6
-        //         flex flex-col justify-between
-        //         relative
-        //         md:block
-        //     "
-        //     style={{
-        //         filter: `blur(${blur}px)`,
-        //         opacity: fade,
-        //     }}>
+   return (
+        <section className="
+                min-h-dvh bg-[#eeeee0] px-6
+                flex flex-col justify-between
+                relative
+                md:block
+            "
+            style={{
+                filter: `blur(${blur}px)`,
+                opacity: fade,
+            }}>
 
-        //     <div className="
-        //         pt-[28vh] mx-auto text-center max-w-[90%]
+            <div className="
+                pt-[28vh] mx-auto text-center max-w-[90%]
 
-        //         sm:max-w-[70%]
+                sm:max-w-[70%]
 
-        //         md:absolute md:top-[4vh]
-        //         md:right-15
-        //         md:max-w-[860px]
-        //         md:text-right
-        //     ">
-        //         <p className="
-        //             font-poppins font-light tracking-tighter
-        //             text-[clamp(1.2rem,2.2vw,3.4rem)]
-        //         ">
-        //             I like figuring out how things work. Whether it's gaming, code, tools, or some random idea that shows up at 2 a.m. If something doesn't make sense, I'll probably poke at it until it does.
-        //         </p>
-        //     </div>
+                md:absolute md:top-[4vh]
+                md:right-15
+                md:max-w-[860px]
+                md:text-right
+            ">
+                <p className="
+                    font-poppins font-light tracking-tighter
+                    text-[clamp(1.2rem,2.2vw,3.4rem)]
+                ">
+                    I like figuring out how things work. Whether it's gaming, code, tools, or some random idea that shows up at 2 a.m. If something doesn't make sense, I'll probably poke at it until it does.
+                </p>
+            </div>
 
-        //     <div className="
-        //         w-full flex justify-center pb-[env(safe-area-inset-bottom)]
-        //         md:absolute md:bottom-[-4vh] 
-        //         md:left-[-1px] md:justify-start
-        //     ">
-        //         <h1 className="
-        //             font-serifbrand font-bold leading-none tracking-tighter
-        //             text-[clamp(8.5rem,22vw,45rem)]
-        //         ">davids</h1>
-        //     </div>
-        // </section>
-//    )
+            <div className="
+                w-full flex justify-center pb-[env(safe-area-inset-bottom)]
+                md:absolute md:bottom-[-4vh] 
+                md:left-[-1px] md:justify-start
+            ">
+                <h1 className="
+                    font-serifbrand font-bold leading-none tracking-tighter
+                    text-[clamp(8.5rem,22vw,45rem)]
+                ">davids</h1>
+            </div>
+        </section>
+   )
 }
